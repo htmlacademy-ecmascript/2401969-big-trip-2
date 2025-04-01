c17c1c0b6f610c955a0d28f94bdc1265aadb662a
vikkont79 <vvikkont79@yandex.ru>
2025-04-01 11:49:08 +0300

    Реализовал обновление данных на сервере

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -23,6 +23,7 @@ export default class PointsModel extends Observable {
     try {
       const points = await this.#pointsApiService.points;
       this.#points = points.map(this.#adaptToClient);
+      console.log(this.#points);
     } catch(err) {
       this.#points = [];
     }
@@ -32,7 +33,6 @@ export default class PointsModel extends Observable {
 
   get newPoint() {
     return {
-      id: 'new',
       basePrice: 0,
       dateFrom: dayjs().toISOString(),
       dateTo: dayjs().toISOString(),
@@ -43,20 +43,25 @@ export default class PointsModel extends Observable {
     };
   }
 
-  updatePoint(updateType, update) {
+  async updatePoint(updateType, update) {
     const index = this.#points.findIndex((point) => point.id === update.id);
 
     if (index === -1) {
       throw new Error('Can\'t update unexisting point');
     }
 
-    this.#points = [
-      ...this.#points.slice(0, index),
-      update,
-      ...this.#points.slice(index + 1),
-    ];
-
-    this._notify(updateType, update);
+    try {
+      const response = await this.#pointsApiService.updatePoint(update);
+      const updatedPoint = this.#adaptToClient(response);
+      this.#points = [
+        ...this.#points.slice(0, index),
+        updatedPoint,
+        ...this.#points.slice(index + 1),
+      ];
+      this._notify(updateType, updatedPoint);
+    } catch(err) {
+      throw new Error('Can\'t update point');
+    }
   }
 
   addPoint(updateType, update) {
